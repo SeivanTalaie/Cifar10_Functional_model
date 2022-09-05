36d994073e94f331f1370c563ac14d47f01257a3
Seivan <seivantalaie7959@gmail.com>
2022-09-05 18:47:29 +0430

    Plot the Results

--- Single_Classification.py
+++ Single_Classification.py
@@ -55,3 +55,7 @@ plot_loss = llp.PlotLossesKeras()
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 history=model.fit(x_train, y_train, batch_size=128,
  epochs=15, validation_data=(x_test,y_test), callbacks=[plot_loss])
+
+# %% Results :
+import pandas as pd 
+pd.DataFrame(history.history).plot()
