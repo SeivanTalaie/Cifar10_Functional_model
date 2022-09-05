e73964c420d561c5ce189b9fc7d96041ecf5337b
Seivan <seivantalaie7959@gmail.com>
2022-09-05 18:39:19 +0430

    Data pre-processed

--- Single_Classification.py
+++ Single_Classification.py
@@ -16,3 +16,8 @@ import pandas as pd
 # %% Load the data :
 (x_train , y_train) , (x_test , y_test) = cifar10.load_data()
 num_classes=10
+
+#%% Pre-processing the data : 
+x_train=x_train.astype("float32") / 255.0
+x_test=x_test.astype("float32") / 255.0
+
